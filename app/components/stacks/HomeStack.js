80f9d16e41c11974c26a0ae73ddf370158234621
Filip Vojtěch <filip.vojtech@outlook.com>
2020-05-26 21:16:22 +0200

    Removed developer log out button

--- app/components/stacks/HomeStack.js
+++ app/components/stacks/HomeStack.js
@@ -1,27 +1,15 @@
-import React, {useContext} from 'react';
+import React from 'react';
 import {createStackNavigator} from '@react-navigation/stack';
 import HomeScreen from '../../screens/HomeScreen';
-import {Text, TouchableOpacity} from 'react-native';
-import {AuthContext} from '../AuthProvider';
 
 //Navigační stack pro domovskou obrazovku
 export const HomeStack = ({}) => {
     const Stack = createStackNavigator();
-    const {logout} = useContext(AuthContext);
 
     return (
         <Stack.Navigator>
             <Stack.Screen
-                options={{
-                    headerRight: () => {
-                        return (
-                            <TouchableOpacity style={{marginRight: 10}} onPress={() => logout()}>
-                                <Text>Log Out</Text>
-                            </TouchableOpacity>
-                        );
-                    },
-                    title: false,
-                }}
+                options={{headerShown: false}}
                 name={'Home'}
                 component={HomeScreen}
             />
