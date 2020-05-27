46ade45fc4438ba50824d2def72bf45239252102
Filip Vojtěch <filip.vojtech@outlook.com>
2020-05-27 18:08:03 +0200

    Applied theme color on buttons

--- app/screens/LoginScreen.js
+++ app/screens/LoginScreen.js
@@ -5,17 +5,19 @@ import {
 } from 'react-native';
 import {Center} from '../components/Center';
 import {AuthContext} from '../components/AuthProvider';
+import {ThemeContext} from '../components/ThemesContext';
 
 //Obrazovka s odkazy na přihlašovací, nebo registrační obrazovky
 export default function LoginScreen({navigation}) {
+    const {mainColor} = useContext(ThemeContext);
     const {login, loginChild} = useContext(AuthContext);
 
     return (
         <Center>
             <View>
-                <Button title={'Přihlásit se jako dospělý'} onPress={() => login()}/>
-                <Button title={'Přihlásit se jako dítě'} onPress={() => loginChild()}/>
-                <Button title={'Registrovat se'} onPress={() => navigation.navigate('Register')}/>
+                <Button color={mainColor} title={'Přihlásit se jako dospělý'} onPress={() => login()}/>
+                <Button color={mainColor} title={'Přihlásit se jako dítě'} onPress={() => loginChild()}/>
+                <Button color={mainColor} title={'Registrovat se'} onPress={() => navigation.navigate('Register')}/>
             </View>
         </Center>
     );
--- app/screens/RegisterScreen.js
+++ app/screens/RegisterScreen.js
@@ -1,23 +1,24 @@
-import React, {useState} from 'react';
+import React, {useContext, useState} from 'react';
 import {
     View,
     Text,
     Button,
 } from 'react-native';
 import {Center} from '../components/Center';
+import {ThemeContext} from '../components/ThemesContext';
 
 //Registrační obrazovka
 //Registrovat se mohou pouze rodiče (osoby nad 18 let)
 export default function RegisterScreen({navigation}) {
+    const {mainColor} = useContext(ThemeContext);
     const [isShown, setIsShown] = useState(false);
-    const text = isShown ? 'Už jsi' : 'Nejsi zaregistrovaný';
 
     return (
         <Center>
             <View>
-                <Button title={'Zaregistrovate se'} onPress={() => setIsShown(true)}/>
-                <Button title={'Zpět na přihlášení'} onPress={() => navigation.navigate('Login')}/>
-                <Text>{text}</Text>
+                <Button color={mainColor} title={'Zaregistrovate se'} onPress={() => setIsShown(true)}/>
+                <Button color={mainColor} title={'Zpět na přihlášení'} onPress={() => navigation.navigate('Login')}/>
+                <Text>{isShown ? 'Už jsi' : 'Nejsi zaregistrovaný'}</Text>
             </View>
         </Center>
     );
