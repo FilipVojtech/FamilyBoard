7fc7f26df8212e146c11b36e73ff45fad1c1aff4
Filip Vojtěch <filip.vojtech@outlook.com>
2020-05-27 02:28:52 +0200

    Applied theme

--- app/screens/UserScreen.js
+++ app/screens/UserScreen.js
@@ -6,17 +6,19 @@ import {
     StyleSheet,
 } from 'react-native';
 import {AuthContext} from '../components/AuthProvider';
+import {ThemeContext} from '../components/Themes';
 
 //TODO: Obrazovka uživatele
 //Obsahuje profilový obrázek, jméno, ostatní členy rodiny, odkaz na obrazovku zdraví
 export default function UserScreen({navigation}) {
+    const {mainColor} = useContext(ThemeContext);
     const {user, logout} = useContext(AuthContext);
 
     return (
         <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
             <Text style={style.name}>{user.name} {user.surname}</Text>
-            <Button title='Zdravotní karta' onPress={() => navigation.navigate('Health')}/>
-            <Button title='Odhlásit se' onPress={() => logout()}/>
+            <Button title='Zdravotní karta' color={mainColor} onPress={() => navigation.navigate('Health')}/>
+            <Button title='Odhlásit se' color={mainColor} onPress={() => logout()}/>
         </View>
     );
 }
