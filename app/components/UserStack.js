de1e03ae16a346b446dd7928bdeb9d638f423b72
Filip Vojtěch <filip.vojtech@outlook.com>
2020-05-29 17:50:28 +0200

    Beautify User Screen

--- app/components/UserStack.js
+++ app/components/UserStack.js
@@ -1,16 +1,33 @@
-import React from 'react';
+import React, {useContext} from 'react';
 import {createStackNavigator} from '@react-navigation/stack';
 import UserScreen from '../screens/UserScreen';
 import HealthScreen from '../screens/HealthScreen';
+import {AuthContext} from './AuthProvider';
+import {Text, TouchableOpacity} from 'react-native';
 
 //Navigační stack pro záložku uživatele
 export const UserStack = ({}) => {
     const Stack = createStackNavigator();
+    const {user, logout} = useContext(AuthContext);
 
     return (
         <Stack.Navigator>
-            <Stack.Screen options={{headerShown: false}} name={'User'} component={UserScreen}/>
-            <Stack.Screen name={'Health'} component={HealthScreen}/>
+            <Stack.Screen
+                options={{headerTitle: null, headerRight: () => headerLogOut(logout)}}
+                name={'User'}
+                component={UserScreen}/>
+            <Stack.Screen
+                options={{headerTitle: `Zdravotní karta ${user.name} ${user.surname}`}}
+                name={'Health'}
+                component={HealthScreen}/>
         </Stack.Navigator>
     );
 };
+
+const headerLogOut = (logout) => (
+    <TouchableOpacity
+        style={{marginRight: 10}}
+        onPress={logout}>
+        <Text style={{fontSize: 16, color: 'red'}}>Odhlásit se</Text>
+    </TouchableOpacity>
+);
--- app/screens/UserScreen.js
+++ app/screens/UserScreen.js
@@ -1,26 +1,30 @@
 import React, {useContext} from 'react';
-import {Button, StyleSheet, Text} from 'react-native';
+import {Button, StyleSheet, Text, View} from 'react-native';
 import {AuthContext} from '../components/AuthProvider';
 import {ThemeContext} from '../components/ThemesContext';
-import {Center} from '../components/Center';
 
 //TODO: Obrazovka uživatele
 //Obsahuje profilový obrázek, jméno, ostatní členy rodiny, odkaz na obrazovku zdraví
 export default function UserScreen({navigation}) {
     const {mainColor} = useContext(ThemeContext);
-    const {user, logout} = useContext(AuthContext);
+    const {user} = useContext(AuthContext);
 
     return (
-        <Center>
+        <View style={style.container}>
             <Text style={style.name}>{user.name} {user.surname}</Text>
             <Button title='Zdravotní karta' color={mainColor} onPress={() => navigation.navigate('Health')}/>
-            <Button title='Odhlásit se' color={mainColor} onPress={() => logout()}/>
-        </Center>
+        </View>
     );
 }
 
 const style = StyleSheet.create({
+    container: {
+        padding: 10,
+    },
     name: {
-        fontSize: 36,
+        textAlignVertical: 'top',
+        textAlign: 'center',
+        fontSize: 30,
+        marginBottom: 10,
     },
 });
