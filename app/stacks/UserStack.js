949e5c8807df44a7ed23e7b2acdbc5cc8dca83b6
PolygonUnicorn <filip.vojtech@outlook.com>
2020-10-19 00:52:56 +0200

    Translate UserStack

--- app/stacks/UserStack.js
+++ app/stacks/UserStack.js
@@ -4,11 +4,20 @@ import UserScreen from '../screens/UserScreen';
 import HealthScreen from '../screens/HealthScreen';
 import {AuthContext} from '../contexts/AuthProvider';
 import {Text, TouchableOpacity} from 'react-native';
+import {LanguageContext} from "../contexts/Languages";
 
 //Navigační stack pro záložku uživatele
 export const UserStack = ({}) => {
     const Stack = createStackNavigator();
     const {user, logout} = useContext(AuthContext);
+    const t = useContext(LanguageContext);
+    const healthCardTitle = () => {
+        if (t.currentLanguage === 'cs') {
+            return `${t.healthCard} ${user.name} ${user.surname}`;
+        } else if (t.currentLanguage === 'en') {
+            return `${user.name} ${user.surname}'s ${t.healthCard.toLowerCase()}`;
+        }
+    };
 
     /**
      * Obrazovka aktivní uživatele,
@@ -19,21 +28,21 @@ export const UserStack = ({}) => {
     return (
         <Stack.Navigator>
             <Stack.Screen
-                options={{headerTitle: null, headerRight: () => headerLogOut(logout)}}
+                options={{headerTitle: null, headerRight: () => headerLogOut(logout, t)}}
                 name={'User'}
                 component={UserScreen}/>
             <Stack.Screen
-                options={{headerTitle: `Zdravotní karta ${user.name} ${user.surname}`}}
+                options={{headerTitle: healthCardTitle()}}
                 name={'Health'}
                 component={HealthScreen}/>
         </Stack.Navigator>
     );
 };
 
-const headerLogOut = (logout) => (
+const headerLogOut = (logout, t) => (
     <TouchableOpacity
         style={{padding: 10, marginRight: 5}}
         onPress={logout}>
-        <Text style={{fontSize: 16, color: 'red'}}>Odhlásit se</Text>
+        <Text style={{fontSize: 16, color: 'red'}}>{t.logout}</Text>
     </TouchableOpacity>
 );
