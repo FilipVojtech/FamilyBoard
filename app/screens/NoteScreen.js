83c07c9ebcfbc4f7098cb1113b6959e4dd433969
PolygonUnicorn <filip.vojtech@outlook.com>
2020-11-02 11:39:41 +0100

    Translate Note Screen

--- app/screens/NoteScreen.js
+++ app/screens/NoteScreen.js
@@ -2,16 +2,19 @@ import React, {useContext} from 'react';
 import {Button} from 'react-native';
 import NoteList from '../components/NoteList';
 import {ThemeContext} from '../contexts/ThemesContext';
+import {LanguageContext} from "../contexts/Languages";
 
 // Obrazovka obsahující seznam všech poznámek a tlačítko pro přidání poznámky
 // Nic zásadního se zde neděje
 export default function NoteScreen({navigation}) {
     const {mainColor} = useContext(ThemeContext);
+    const t = useContext(LanguageContext);
+
     return (
         <React.Fragment>
-            <NoteList navigation={navigation}/>
+            <NoteList navigation={navigation} languageText={t}/>
             <Button
-                title={'Přidat poznámku'}
+                title={t.addNote}
                 onPress={() => navigation.navigate('Add')}
                 color={mainColor}
             />
