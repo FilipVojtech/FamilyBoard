23bdd5d061d790556caf810e5e99ea91fde9a18c
PolygonUnicorn <filip.vojtech@outlook.com>
2020-11-02 12:05:37 +0100

    Translate Note Stack

--- app/stacks/NotesStack.js
+++ app/stacks/NotesStack.js
@@ -1,18 +1,20 @@
-import React from 'react';
+import React, {useContext} from 'react';
 import {createStackNavigator} from '@react-navigation/stack';
 import NoteScreen from '../screens/NoteScreen';
 import NoteAddScreen from '../screens/NoteAddScreen';
 import NoteEditScreen from '../screens/NoteEditScreen';
+import {LanguageContext} from "../contexts/Languages";
 
 //Navigační stack pro záložku poznámek
 export const NotesStack = ({}) => {
     const Stack = createStackNavigator();
+    const t = useContext(LanguageContext);
 
     return (
         <Stack.Navigator>
             <Stack.Screen options={{headerShown: false}} name={'Notes'} component={NoteScreen}/>
-            <Stack.Screen name={'Add'} options={{title: 'Přidat poznámku'}} component={NoteAddScreen}/>
-            <Stack.Screen name={'Edit'} options={{title: 'Upravit poznámku'}} component={NoteEditScreen}/>
+            <Stack.Screen name={'Add'} options={{title: t.addNote}} component={NoteAddScreen}/>
+            <Stack.Screen name={'Edit'} options={{title: t.editNote}} component={NoteEditScreen}/>
         </Stack.Navigator>
     );
 };
