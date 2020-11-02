6f57dd9d4ff7285545133be4ed58e41eb76fc1a3
PolygonUnicorn <filip.vojtech@outlook.com>
2020-11-02 08:53:32 +0100

    Translated Login Screen

--- app/screens/LoginScreen.js
+++ app/screens/LoginScreen.js
@@ -1,31 +1,25 @@
 import React, {useContext} from 'react';
-import {
-    Button,
-    Image,
-    View
-} from 'react-native';
+import {Button, Image, View,} from 'react-native';
 import {Center} from '../components/Center';
 import {AuthContext} from '../contexts/AuthProvider';
 import {ThemeContext} from '../contexts/ThemesContext';
-// import {LanguageContext} from "../contexts/LanguagesContext";
+import {LanguageContext} from "../contexts/Languages";
 
 //Obrazovka s odkazy na přihlašovací, nebo registrační obrazovky
 export default function LoginScreen({navigation}) {
     const {mainColor} = useContext(ThemeContext);
-    const {login, loginChild, loginMother} = useContext(AuthContext);
+    const {login, loginFather, loginChild, loginMother} = useContext(AuthContext);
+    const t = useContext(LanguageContext);
 
     return (
-        <React.Fragment>
         <Center>
-                <Image source={require('../res/img/logo.png')}/>
-                <TextInput
+            <Image source={require('../res/img/logo.png')}/>
             <View>
-                <Button color={mainColor} title={'Přihlásit se jako otec'} onPress={() => login()}/>
-                <Button color={mainColor} title={'Přihlásit se jako matka'} onPress={() => loginMother()}/>
-                <Button color={mainColor} title={'Přihlásit se jako dítě'} onPress={() => loginChild()}/>
-                <Button color={mainColor} title={'Registrovat se'} onPress={() => navigation.navigate('Register')}/>
+                <Button color={mainColor} title={t.loginFather} onPress={() => loginFather()}/>
+                <Button color={mainColor} title={t.loginMother} onPress={() => loginMother()}/>
+                <Button color={mainColor} title={t.loginChild} onPress={() => loginChild()}/>
+                <Button color={mainColor} title={t.register} onPress={() => navigation.navigate('Register')}/>
             </View>
         </Center>
-        </React.Fragment>
     );
 }
--- app/stacks/LoginStack.js
+++ app/stacks/LoginStack.js
@@ -1,16 +1,18 @@
-import React from 'react';
+import React, {useContext} from 'react';
 import {createStackNavigator} from '@react-navigation/stack';
 import LoginScreen from '../screens/LoginScreen';
 import RegisterScreen from '../screens/RegisterScreen';
+import {LanguageContext} from "../contexts/Languages";
 
-export const Login = ({}) => {
+export const LoginStack = ({}) => {
     const Stack = createStackNavigator();
+    const t = useContext(LanguageContext);
 
     //Navigační stack pro Login
     return (
         <Stack.Navigator>
             <Stack.Screen options={{headerShown: false}} name='Login' component={LoginScreen}/>
-            <Stack.Screen options={{headerShown: false}} name='Register' component={RegisterScreen}/>
+            <Stack.Screen options={{headerTitle:t.createAccount}} name='Register' component={RegisterScreen}/>
         </Stack.Navigator>
     );
 };
