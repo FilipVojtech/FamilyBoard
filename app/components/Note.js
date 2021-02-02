3145ae52bacb4424faf57baff285f405468c8e16
PolygonUnicorn <filip.vojtech@outlook.com>
2021-02-02 17:18:23 +0100

    Create Note.js Move code from NoteList.js to Note.js and NoteScreen.js Remove NoteList.js
    Took 2 minutes

---
+++ app/components/Note.js
@@ -0,0 +1,130 @@
+import {Alert, StyleSheet, Text, TouchableOpacity, View} from "react-native";
+import AsyncStorage from "@react-native-community/async-storage";
+import Icon from "react-native-vector-icons/MaterialCommunityIcons";
+import React, {useContext, useState} from "react";
+import {AuthContext} from "../contexts/AuthProvider";
+import {ThemeContext} from "../contexts/ThemesContext";
+import {LanguageContext} from "../contexts/Languages";
+
+export const Note = (props) => {
+    const {mainColor} = useContext(ThemeContext);
+    const {user} = useContext(AuthContext);
+    const t = useContext(LanguageContext);
+    const [notes, setNotes] = useState([]);
+    const {navigation, item} = props;
+
+    AsyncStorage.getItem('notes')
+        .then(value => {
+            setNotes(JSON.parse(value));
+        });
+
+    function deleteNote(item) {
+        if (user.isParent || item.user.UUID === user.UUID) {
+            Alert.alert(
+                'Jsi si jistý?',
+                'Opravdu chceš vymazat tuto poznámku? \nNelze ji pak vrátit zpět',
+                [
+                    {
+                        text: 'Smazat',
+                        onPress: () => {
+                            for (let i = 0; i < notes.length; i++) {
+                                if (notes[i].id === item.id) {
+                                    notes.splice(i, 1);
+                                }
+                            }
+                            AsyncStorage.setItem('notes', JSON.stringify(notes));
+                        },
+                    },
+                    {
+                        text: 'Nemazat!',
+                    },
+                ],
+            );
+        } else {
+            Alert.alert(
+                'Nedostatečná opravnění',
+                'Tuhle poznámku nemůžeš smazat, protože ji vytvořil rodič',
+            );
+        }
+    }
+
+    return (
+        <TouchableOpacity
+            disabled={false}
+            style={style.note}
+            onPress={() => navigation.navigate('Edit', item)}
+            onLongPress={() => deleteNote(item)}>
+            <View style={style.noteText}>
+                <Text style={style.unimportant}>
+                    {`${t.added}: ${item.dateAdded}`}
+                </Text>
+                {item.title ? <Text style={style.title}>{item.title}</Text> : null}
+                <Text style={style.unimportant}>
+                    {t.by.toLowerCase() ? t.by.toLowerCase() + " " : null}{item.user.name} {item.user.surname}
+                </Text>
+                <Text style={style.text}>{item.text}</Text>
+            </View>
+            <TouchableOpacity
+                style={style.noteSettings}
+                onPress={() => {
+                    for (let i = 0; i < notes.length; i++) {
+                        if (notes[i].id === item.id) {
+                            notes[i].isDone = !notes[i].isDone;
+                        }
+                    }
+                    AsyncStorage.setItem('notes', JSON.stringify(notes));
+                }}>
+                <Icon
+                    name={
+                        item.isDone
+                            ? 'checkbox-marked-circle-outline'
+                            : 'checkbox-blank-circle-outline'
+                    }
+                    size={34}
+                    color={mainColor}
+                />
+            </TouchableOpacity>
+        </TouchableOpacity>
+    )
+}
+
+const style = StyleSheet.create({
+    center: {
+        textAlign: 'center',
+    },
+    line: {
+        borderTopWidth: 2,
+        borderColor: '#dcdcdc',
+    },
+    note: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        flex: 1,
+        justifyContent: 'space-around',
+        padding: 10,
+    },
+    title: {
+        fontSize: 18,
+        fontWeight: 'bold',
+    },
+    text: {
+        fontSize: 16,
+    },
+    unimportant: {
+        fontSize: 12,
+        fontStyle: 'italic',
+    },
+    noteText: {
+        flex: 1,
+        alignSelf: 'center',
+        flexGrow: 1,
+        flexShrink: 0,
+        alignContent: 'stretch',
+    },
+    noteSettings: {
+        alignSelf: 'flex-start',
+        marginTop: 10,
+        marginRight: 10,
+    },
+});
+
--- app/screens/NoteScreen.js
+++ app/screens/NoteScreen.js
@@ -1,18 +1,43 @@
-import React, {useContext} from 'react';
-import {Button} from 'react-native';
-import NoteList from '../components/NoteList';
+import React, {useContext, useState} from 'react';
+import {Button, StyleSheet, Text} from 'react-native';
 import {ThemeContext} from '../contexts/ThemesContext';
 import {LanguageContext} from "../contexts/Languages";
+import AsyncStorage from "@react-native-community/async-storage";
+import {SwipeListView} from "react-native-swipe-list-view";
+import {Center} from "../components/Center";
+import {Note} from "../components/Note";
 
 // Obrazovka obsahující seznam všech poznámek a tlačítko pro přidání poznámky
 // Nic zásadního se zde neděje
 export default function NoteScreen({navigation}) {
     const {mainColor} = useContext(ThemeContext);
+    const [notes, setNotes] = useState([]);
     const t = useContext(LanguageContext);
 
-    return (
+    AsyncStorage.getItem('notes')
+        .then(value => {
+            setNotes(JSON.parse(value));
+        });
+
+    if (notes == null || notes.length === 0) return (
         <React.Fragment>
-            <NoteList navigation={navigation} languageText={t}/>
+            <Center>
+                <Text style={style.center}>{t.notesNothing}</Text>
+            </Center>
+            <Button
+                title={t.addNote}
+                onPress={() => navigation.navigate('Add')}
+                color={mainColor}
+            />
+        </React.Fragment>
+    );
+    else return (
+        <React.Fragment>
+            <Button title={'Log notes'} onPress={() => console.log(notes)}/>
+            <SwipeListView
+                data={notes}
+                renderItem={({item}) => <Note t={t} item={item} navigation={navigation}/>}
+            />
             <Button
                 title={t.addNote}
                 onPress={() => navigation.navigate('Add')}
@@ -21,3 +46,44 @@ export default function NoteScreen({navigation}) {
         </React.Fragment>
     );
 }
+
+const style = StyleSheet.create({
+    center: {
+        textAlign: 'center',
+    },
+    line: {
+        borderTopWidth: 2,
+        borderColor: '#dcdcdc',
+    },
+    note: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        flex: 1,
+        justifyContent: 'space-around',
+        padding: 10,
+    },
+    title: {
+        fontSize: 18,
+        fontWeight: 'bold',
+    },
+    text: {
+        fontSize: 16,
+    },
+    unimportant: {
+        fontSize: 12,
+        fontStyle: 'italic',
+    },
+    noteText: {
+        flex: 1,
+        alignSelf: 'center',
+        flexGrow: 1,
+        flexShrink: 0,
+        alignContent: 'stretch',
+    },
+    noteSettings: {
+        alignSelf: 'flex-start',
+        marginTop: 10,
+        marginRight: 10,
+    },
+});
+
