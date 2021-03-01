b6e61a260e057feb040973e00f2094ac5ce0eef5
PolygonUnicorn <filip.vojtech@outlook.com>
2021-03-01 11:51:21 +0100

    Add user picture placeholder
    Took 1 hour 7 minutes

--- app/screens/UserScreen.js
+++ app/screens/UserScreen.js
@@ -3,6 +3,7 @@ import {Button, StyleSheet, Text, View} from 'react-native';
 import {AuthContext} from '../contexts/AuthProvider';
 import {ThemeContext} from '../contexts/ThemesContext';
 import {LanguageContext} from "../contexts/Languages";
+import Icon from "react-native-vector-icons/MaterialCommunityIcons";
 
 //TODO: Obrazovka uživatele
 //Obsahuje profilový obrázek, jméno, ostatní členy rodiny, odkaz na obrazovku zdraví
@@ -13,6 +14,7 @@ export default function UserScreen({navigation}) {
 
     return (
         <View style={style.container}>
+            <Icon name='account-circle' color={mainColor} size={192} style={style.profilePic}/>
             <Text style={style.name}>{user.name} {user.surname}</Text>
             <Button title={t.healthCard} color={mainColor} onPress={() => navigation.navigate('Health')}/>
         </View>
@@ -28,5 +30,8 @@ const style = StyleSheet.create({
         textAlign: 'center',
         fontSize: 30,
         marginBottom: 10,
+    },
+    profilePic: {
+        alignSelf: 'center',
     },
 });
